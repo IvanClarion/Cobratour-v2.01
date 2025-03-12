56f4c77636afd16072a0081c02964a05a86d51af
unknown <ivandaleclarion@gmail.com>
2025-03-12 14:54:22 -0800

    spline bug fix

--- src/Client/ClientExplore.jsx
+++ src/Client/ClientExplore.jsx
@@ -3,11 +3,7 @@ import Spline from '@splinetool/react-spline';
 function ClientExplore() {
   return (
     <section className='h-screen'>
-         <main>
-      <Spline
-        scene="https://prod.spline.design/qS6bUQoGN1zbpPsO/scene.splinecode" 
-      />
-    </main>
+          <Spline scene="https://prod.spline.design/qS6bUQoGN1zbpPsO/scene.splinecode" />
   
     </section>
   );
