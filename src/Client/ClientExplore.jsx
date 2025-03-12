ee374ddbec41f5e4ecb4ea331166ce866d94867d
unknown <ivandaleclarion@gmail.com>
2025-03-11 21:41:37 -0800

    spline updated

--- src/Client/ClientExplore.jsx
+++ src/Client/ClientExplore.jsx
@@ -3,7 +3,11 @@ import Spline from '@splinetool/react-spline';
 function ClientExplore() {
   return (
     <section className='h-screen'>
-         <Spline scene="https://prod.spline.design/qS6bUQoGN1zbpPsO/scene.splinecode" />
+         <main>
+      <Spline
+        scene="https://prod.spline.design/qS6bUQoGN1zbpPsO/scene.splinecode" 
+      />
+    </main>
   
     </section>
   );
