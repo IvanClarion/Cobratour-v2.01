28658849cd0716f12cfbd6d352e472c00a97c1a5
IvanClarion <ivandaleclarion@gmail.com>
2025-03-18 09:52:55 -0800

    bug security

--- src/ControlAdmin/ControlNavbar.jsx
+++ src/ControlAdmin/ControlNavbar.jsx
@@ -2,14 +2,16 @@ import DashboardIcon from './AdminIcons/Dashboard.svg';
 import AdminLogo from './AdminLogo/admin-logo.svg';
 import AccountIcon from './AdminIcons/Account.svg';
 import LogoutIcon from './AdminIcons/logout.svg';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import HmaburgerIcon from './AdminIcons/hamburger.svg';
 import { useState, useEffect } from 'react';
-
+import { signOut } from "firebase/auth";
+import { auth } from '../config/firebase'; // Import your auth object
 
 function ControlNavbar() {
   const [openNav, setOpenNav] = useState(false);
   const [isMobileView, setIsMobileView] = useState(window.innerWidth <= 1023);
+  const navigate = useNavigate(); // Initialize navigate
 
   useEffect(() => {
     const handleResize = () => {
@@ -20,43 +22,50 @@ function ControlNavbar() {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      localStorage.clear();
+      sessionStorage.clear();
+      navigate('/controlLogin');
+    } catch (error) {
+      console.log("Error", error);
+    }
+  };
+
   return (
-    
-      <aside >
-        
-        {isMobileView && (
-          <div className="general-nav" onClick={() => setOpenNav((prev) => !prev)}>
-            <img src={HmaburgerIcon} alt="Menu" className="lg:hidden" />
-          </div>
-        )}
+    <aside>
+      {isMobileView && (
+        <div className="general-nav" onClick={() => setOpenNav((prev) => !prev)}>
+          <img src={HmaburgerIcon} alt="Menu" className="lg:hidden" />
+        </div>
+      )}
 
-        
-        {(openNav || !isMobileView) && (
-          
-          
-            <nav className="admin-navbar">
-              <ul className="admin-ul">
-                <img src={AdminLogo} alt="logo" className="p-2" />
+      {(openNav || !isMobileView) && (
+        <nav className="admin-navbar">
+          <ul className="admin-ul">
+            <img src={AdminLogo} alt="logo" className="p-2" />
 
-                <li>
-                  <img src={DashboardIcon} alt="dashboard" className="w-5" />
-                  <Link to="/controlDashboard">Dashboard</Link>
-                </li>
+            <li>
+              <img src={DashboardIcon} alt="dashboard" className="w-5" />
+              <Link to="/controlDashboard">Dashboard</Link>
+            </li>
 
-                <li>
-                  <img src={AccountIcon} alt="account" className="w-5" />
-                  <Link to="/controlAccount">Account</Link>
-                </li>
+            <li>
+              <img src={AccountIcon} alt="account" className="w-5" />
+              <Link to="/controlAccount">Account</Link>
+            </li>
 
-                <li className="absolute bottom-5">
-                  <img src={LogoutIcon} alt="logout" className="w-5" />
-                  <Link to="/loginControl">Logout</Link>
-                </li>
-              </ul>
-            </nav>
-          
-        )}
-      </aside>
+            <li className="absolute bottom-5">
+              <button type='button' onClick={handleLogout} className="flex items-center gap-1 ">
+                <img src={LogoutIcon} alt="Logout" className="w-5" />
+                Logout
+              </button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </aside>
   );
 }
 
